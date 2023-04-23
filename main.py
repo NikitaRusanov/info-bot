f4e5ea3898a33c39fc18c38079f4731fbb570218
Nikita <nipe322@gmail.com>
2023-04-23 17:43:36 +0300

    Add simple response to /start

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from telebot import TeleBot
 from config import BOT_TOKEN
 
+bot = TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def start_message(message):
+    bot.reply_to(message, 'It is bot, that can send you message, when stock reach some price')
+
+
+bot.infinity_polling()
