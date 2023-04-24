afc9c5bcb013fc248e924899c6b298400e3a84d9
Nikita <nipe322@gmail.com>
2023-04-24 20:31:03 +0300

    Add message handler interface

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,10 +1,19 @@
 from telebot import TeleBot
 from telebot.types import Message
+from abc import ABC, abstractmethod
+from typing import Optional
 
 
-def start_command(message: Message, bot: TeleBot):
-    bot.reply_to(message, 'It is a bot, that can send you message, when stock reach some price')
+class BaseHandler(ABC):
+    @abstractmethod
+    def __call__(self, message: Message, bot: Optional[TeleBot]) -> None:
+        raise NotImplemented
+
+
+class StartHandler(BaseHandler):
+    def __call__(self, message: Message, bot: Optional[TeleBot]) -> None:
+        bot.reply_to(message, 'It is a bot, that can send you message, when stock reach some price')
 
 
 def setup_handlers(bot: TeleBot):
-    bot.register_message_handler(callback=start_command, commands=['help', 'start'], pass_bot=True)
+    bot.register_message_handler(callback=StartHandler(), commands=['help', 'start'], pass_bot=True)
